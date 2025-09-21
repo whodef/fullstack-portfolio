1723a3e33964e51e295f23a328eca2df93af0d6b
Tatiana <7266512+whodef@users.noreply.github.com>
2025-09-21 23:38:05 +0300

    feat: New

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from 'next'
+
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
   output: 'export',
-  basePath: '',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
+
+  distDir: 'docs',
 }
+
 export default nextConfig;
